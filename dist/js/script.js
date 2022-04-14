fb29f01005e1dede5759c7fe83bf3611aa099ce7
andrMaulana <andryymaulana2000@gmail.com>
2022-04-14 16:34:47 +0700

    menambahkan fitur darkMode pada portofolio

--- dist/js/script.js
+++ dist/js/script.js
@@ -29,3 +29,23 @@ window.addEventListener('click', function (e) {
         navMenu.classList.add('hidden');
     }
 });
+
+// dark mode
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+darkToggle.addEventListener('click', function(){
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark'
+    }else{
+        html.classList.remove('dark')
+        localStorage.theme = 'light'
+    }
+})
+
+// memindahkan toggle sesuai tema 
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
